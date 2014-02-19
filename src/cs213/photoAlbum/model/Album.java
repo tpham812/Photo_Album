7fe3d7d489c07de0c8e4321a838ce33963e9506c
Brett Bertoni <bbertoni1@gmail.com>
2014-02-19 22:40:08 +0100

    committing project

---
+++ src/cs213/photoAlbum/model/Album.java
@@ -0,0 +1,38 @@
+package cs213.photoAlbum.model;
+
+/**
+ * @author brett
+ * 
+ *
+ */
+public class Album {
+	private String name; 
+	private int photos;
+	private List<Photo> photoList; 
+	
+	public Album(String name, int photos){
+		
+	}
+	
+
+	
+	public void addPhoto(){
+		
+	}
+	
+	public void deletePhoto(){
+		
+	}
+	
+	public String renamePhoto(){
+		
+	}
+	
+	public List<Photo> getPhotos(){
+		return photoList; 
+	}
+	
+	public String toString(){
+		return name; 
+	}
+}
---
+++ src/cs213/photoAlbum/model/User.java
@@ -0,0 +1,63 @@
+package cs213.photoAlbum.model;
+
+import java.awt.List;
+
+/**
+ * @author brett
+ *  New class User which will include the unique ID for each user, their full name, and the album(s) of the user
+ *  
+*/
+public class User {
+	private String id = ""; 
+	private String fname = "";
+	List<Album> albumList; 
+	
+	/**
+	 * Constructor for the User object of the User class
+	 * @author brett
+	 * @param id The users unique string ID
+	 * @param fname The users full name
+	 */
+	
+	public User(String id , String fname){
+	 this.id = id; 
+	 this.fname = fname;
+	}
+	
+	/**
+	 * Adds new album to the list of albums
+	 * @author brett
+	 * 
+	 * 
+	 */
+	public void addAlbum(){
+		
+	}
+	/**
+	 * deletes album associated to the user
+	 * @author brett
+	 * 
+	 * 
+	 */
+	public void deleteAlbum(){ 
+		
+	}
+	
+	/**
+	 * Renames the specified album
+	 * @author brett
+	 * @param album The album title which is to be renamed
+	 * @return album The newly created album title
+	 */
+	public String rename(String album){
+		return album; 
+	}
+	
+	/**
+	 * @author brett
+	 * @return id The users unique string ID 
+	 */
+	public String toString(){
+		return id; 
+	}
+}
