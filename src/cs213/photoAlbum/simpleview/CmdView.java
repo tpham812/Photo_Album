aea29e662ac7f6c248a40dd29043c1295450d0a5
bbertoni <bbertoni@localhost>
2014-03-02 11:58:31 +0100

    updated CmdView

--- src/cs213/photoAlbum/simpleview/CmdView.java
+++ src/cs213/photoAlbum/simpleview/CmdView.java
@@ -1,5 +1,12 @@
 package cs213.photoAlbum.simpleview;
 
 public class CmdView {
+	public static void main(String[] args){
+		if (args.length == 0){
+			System.out.println("Please input a command");
+		}
+		
+		
+	}
 
 }
