cd62093e2dd00c154fb627ea88ba0dcbcdfe0f97
bbertoni <bbertoni@localhost>
2014-03-04 14:05:34 +0100

    createalbum updated

--- src/cs213/photoAlbum/control/AlbumController.java
+++ src/cs213/photoAlbum/control/AlbumController.java
@@ -10,74 +10,74 @@ public class AlbumController implements IAlbumController {
 
 	@Override
 	public List<Album> listAlbums(User user) {
-			return user.getAlbums(); 
+		return user.getAlbums();
 	}
 
 	@Override
 	public boolean createAlbum(String albumName, User user) {
-		Album albm = new Album(albumName, 0, null);
-		user.albumList.add(albm); 
-		if (user.albumList == null){
-			return false; 
-		} else {
-		return true; 
+		Album album= new Album(albumName,0,null);
+		user.addAlbum(album);
+		if (user.albumList == null) {
+			return false;
+		}
+		return true;
 	}
-	}
+
 	@Override
 	public boolean deleteAlbum(String albumName, User user) {
-		user.deleteAlbum(albumName); 
-		return true; 
+		user.deleteAlbum(albumName);
+		return true;
 	}
 
 	@Override
 	public List<Photo> listPhotos(String albumName, User user) {
-		 for (int i = 0; i < user.albumList.size(); i++){
-			 if (albumName.equals(user.albumList.get(i).getAlbumName())){
-				 return user.albumList.get(i).getPhotos(); 
-				 }
-		 }
-		
-		return null;  
-		
+		for (int i = 0; i < user.albumList.size(); i++) {
+			if (albumName.equals(user.albumList.get(i).getAlbumName())) {
+				return user.albumList.get(i).getPhotos();
+			}
+		}
+
+		return null;
+
 	}
 
 	@Override
-	public boolean addPhoto(String fileName, String caption, String albumName, User user) {
-		Photo photo = new Photo(); 
+	public boolean addPhoto(String fileName, String caption, String albumName,
+			User user) {
+		Photo photo = new Photo();
 		photo.setName(fileName);
-	    photo.setCaption(caption); 
-	    if (photo == null){
-	    	return false;
-	    }
-		
-		for (int i = 0; i < user.albumList.size(); i++){
-			 if (albumName.equals(user.albumList.get(i).getAlbumName())){
-				user.albumList.get(i).photoList.add(photo);
-				 
-				 return true; 
-			 }
-		} 
+		photo.setCaption(caption);
+		if (photo == null) {
 			return false;
+		}
+
+		for (int i = 0; i < user.albumList.size(); i++) {
+			if (albumName.equals(user.albumList.get(i).getAlbumName())) {
+				user.albumList.get(i).photoList.add(photo);
+
+				return true;
+			}
+		}
+		return false;
 	}
-	
 
 	@Override
-	public boolean movePhoto(String fileName, String oldAlbumName, String newAlbumName, User user) {
-		
+	public boolean movePhoto(String fileName, String oldAlbumName,
+			String newAlbumName, User user) {
+
 		return false;
 	}
 
 	@Override
 	public boolean removePhoto(String fileName, String albumName, User user) {
-		for (int i = 0; i < user.albumList.size(); i++){
-			 if (albumName.equals(user.albumList.get(i).getAlbumName())){
-				user.albumList.get(i).photoList.remove(fileName); 
-				return true; 
-		
-		
-			 }
+		for (int i = 0; i < user.albumList.size(); i++) {
+			if (albumName.equals(user.albumList.get(i).getAlbumName())) {
+				user.albumList.get(i).photoList.remove(fileName);
+				return true;
+
+			}
 
 		}
-		return false; 
+		return false;
 	}
 }
--- src/cs213/photoAlbum/model/User.java
+++ src/cs213/photoAlbum/model/User.java
@@ -1,123 +1,141 @@
 package cs213.photoAlbum.model;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
-
 /**
- * Represents the User, containing a unique ID, their full name, and the album(s) of the user
+ * Represents the User, containing a unique ID, their full name, and the
+ * album(s) of the user
  * 
  * @author brett
  **/
 public class User implements Serializable {
-	
-	/**  ID of the user. */
-	private String id; 
-	
-	/**  full name of the user. */
+
+	/** ID of the user. */
+	private String id;
+
+	/** full name of the user. */
 	private String fname;
-	
-	/**  List of albums. */
-	public List<Album> albumList; 
-	
-	/**  User's photos indexed by the name of the file. */
-	Map<String,Photo> photos;
-	 
+
+	/** List of albums. */
+	public List<Album> albumList;
+
+	/** User's photos indexed by the name of the file. */
+	Map<String, Photo> photos;
+
 	/**
 	 * Constructor for the User object of the User class.
-	 *
-	 * @param id The users unique string ID
-	 * @param fname The users full name
-	 */	
-	public User(String id , String fname){
+	 * 
+	 * @param id
+	 *            The users unique string ID
+	 * @param fname
+	 *            The users full name
+	 */
+	public User(String id, String fname) {
 		/** Users ID */
-	 this.id = id; 
-	 	/** Users full name*/
-	 this.fname = fname;
+		this.id = id;
+		/** Users full name */
+		this.fname = fname;
 	}
-	
+
 	/**
 	 * Adds new album to the list of albums.
-	 *
-	 * @param user The user
+	 * 
+	 * @param user
+	 *            The user
 	 */
-	public void addAlbum(User user){
-		
+	public void addAlbum(Album album) {
+		/* check that album adds */
+		if(albumList==null){
+			List<Album> alb =	new ArrayList<Album>();	
+			alb.add(album);
+			this.albumList=alb;
+			System.out.println("album created for user "+ this + ":");
+			System.out.println(album.getAlbumName()); 
+		}
+	/*	else {
+			this.albumList.add(album);
+				System.out.println("album created for user "+ this + ":");
+				System.out.println(album.getAlbumName()); 
+		}
+	*/
 	}
-	
+
 	/**
 	 * deletes album .
 	 */
-	public void deleteAlbum(String albumName){ 
-		
+	public void deleteAlbum(String albumName) {
+
 	}
-	
+
 	/**
 	 * Renames the specified album.
-	 *
-	 * @param album The album title which is to be renamed
+	 * 
+	 * @param album
+	 *            The album title which is to be renamed
 	 * @return album The newly created album title
 	 */
-	public String rename(Album album){
-		return id; 
-		
+	public String rename(Album album) {
+		return id;
+
 	}
-	
+
 	/**
 	 * To string.
-	 *
+	 * 
 	 * @return id The users unique string ID
 	 */
-	public String toString(){
-		return id; 
+	public String toString() {
+		return id;
 	}
-	
-	
+
 	/**
 	 * Sets the users ID.
-	 *
-	 * @param id Users ID
+	 * 
+	 * @param id
+	 *            Users ID
 	 */
-	 public void setUserID(String id){
-		 this.id = id; 
-	 }
-	 
-	 
-	 /**
- 	 * Sets the users full name.
- 	 *
- 	 * @param fname Users full name
- 	 */
-	 public void setUserFullName(String fname){
-		 this.fname = fname; 
-	 }
-	 
-	 /**
- 	 * Returns the users id.
- 	 *
- 	 * @return id users id
- 	 */
-	 public String getUserID(){
-		 return id; 
-	 }
-	 
-	 /**
- 	 * Returns the users full name.
- 	 *
- 	 * @return fname Users full name
- 	 */
-	 public String getUserFullName(){
-		 return fname; 
-	 }
+	public void setUserID(String id) {
+		this.id = id;
+	}
 
-	 public List<Album> getAlbums(){
-		 return albumList; 
-	 }
+	/**
+	 * Sets the users full name.
+	 * 
+	 * @param fname
+	 *            Users full name
+	 */
+	public void setUserFullName(String fname) {
+		this.fname = fname;
+	}
+
+	/**
+	 * Returns the users id.
+	 * 
+	 * @return id users id
+	 */
+	public String getUserID() {
+		return id;
+	}
+
+	/**
+	 * Returns the users full name.
+	 * 
+	 * @return fname Users full name
+	 */
+	public String getUserFullName() {
+		return fname;
+	}
+
+	public List<Album> getAlbums() {
+		return albumList;
+	}
 
 	/**
 	 * Gets the photos.
-	 *
+	 * 
 	 * @return the photos
 	 */
 	public Map<String, Photo> getPhotos() {
@@ -126,12 +144,12 @@ public class User implements Serializable {
 
 	/**
 	 * Sets the photos.
-	 *
-	 * @param photos the photos
+	 * 
+	 * @param photos
+	 *            the photos
 	 */
 	public void setPhotos(Map<String, Photo> photos) {
 		this.photos = photos;
 	}
-	 
-	 
+
 }
