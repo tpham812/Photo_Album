550ac9659526cf18aeb87ffc922c339b4dba47b2
dbas <dbas@localhost>
2014-04-06 13:02:33 -0400

    Hooked up AlbumController

--- src/cs213/photoAlbum/simpleview/ViewContainer.java
+++ src/cs213/photoAlbum/simpleview/ViewContainer.java
@@ -1,17 +1,22 @@
 package cs213.photoAlbum.simpleview;
 
+import java.util.Calendar;
+import java.util.Collection;
+
 import cs213.photoAlbum.control.AlbumController;
 import cs213.photoAlbum.control.IAlbumController;
 import cs213.photoAlbum.control.IPhotoController;
 import cs213.photoAlbum.control.IUserController;
 import cs213.photoAlbum.control.PhotoController;
 import cs213.photoAlbum.control.UserController;
+import cs213.photoAlbum.model.IAlbum;
 import cs213.photoAlbum.model.IUser;
+import cs213.photoAlbum.util.CalendarUtils;
 
 public class ViewContainer {
-	
+
 	/**
-	 * Controller to manage user admin. 
+	 * Controller to manage user admin.
 	 */
 	protected IUserController userController;
 
@@ -20,9 +25,8 @@ public class ViewContainer {
 
 	/** The album controller. */
 	protected IAlbumController albumController;
-	
-	private IUser user;
 
+	private IUser user;
 
 	public void setUser(IUser user) {
 		this.user = user;
@@ -32,19 +36,23 @@ public class ViewContainer {
 
 		this.userController = new UserController();
 		this.photoController = new PhotoController();
-		this.albumController = new AlbumController();	
+		this.albumController = new AlbumController();
 	}
-	
+
 	public IUser login(String userId) {
 
 		user = userController.login(userId);
 		return user;
 	}
-	
-	
+
 	public IUser getUser() {
 		return user;
 	}
-	
+
+	public Collection<IAlbum> listAlbums() {
+		Collection<IAlbum> albums = albumController.listAlbums(user);
+
+		return albums;
+	}
 
 }
