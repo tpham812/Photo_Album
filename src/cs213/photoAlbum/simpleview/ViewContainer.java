2a93023b87a8b6dc4401e9e7455b672d003eba11
dbas <dbas@localhost>
2014-04-11 17:26:25 -0400

    undoing bad merge

--- src/cs213/photoAlbum/simpleview/ViewContainer.java
+++ src/cs213/photoAlbum/simpleview/ViewContainer.java
@@ -1,9 +1,9 @@
 package cs213.photoAlbum.simpleview;
 
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Collection;
 import java.util.List;
-import java.util.SortedSet;
 
 import cs213.photoAlbum.control.AlbumController;
 import cs213.photoAlbum.control.IAlbumController;
@@ -12,8 +12,8 @@ import cs213.photoAlbum.control.IUserController;
 import cs213.photoAlbum.control.PhotoController;
 import cs213.photoAlbum.control.UserController;
 import cs213.photoAlbum.model.IAlbum;
-import cs213.photoAlbum.model.IUser;
 import cs213.photoAlbum.model.IPhoto;
+import cs213.photoAlbum.model.IUser;
 import cs213.photoAlbum.util.CalendarUtils;
 
 public class ViewContainer {
@@ -30,10 +30,10 @@ public class ViewContainer {
 	protected IAlbumController albumController;
 
 	private IUser user;
+	
+	private IAlbum album;
 
-	public void setUser(IUser user) {
-		this.user = user;
-	}
+	protected List<IPhoto> photos;
 
 	public ViewContainer() {
 
@@ -51,66 +51,86 @@ public class ViewContainer {
 	public IUser getUser() {
 		return user;
 	}
-	
+
 	public List<String> listUser() {
-		
+
 		return userController.listUsers();
 	}
+
 	public boolean addUser(String userId, String userName) {
-		
+
 		return userController.addUser(userId, userName);
 	}
-	
+
 	public boolean deleteUser(String userId) {
-		
+
 		return userController.deleteUser(userId);
 	}
-	
+
 	public boolean createAlbum(String albumName) {
-		
+
 		return albumController.createAlbum(albumName, user);
 	}
-	
+
 	public IAlbum getAlbum(String albumName) {
-		
+
 		return albumController.getAlbum(albumName, user);
 	}
-	
+
 	public boolean isAlbumExist(String albumName) {
-		
+
 		return user.containsAlbum(albumName);
 	}
-	
+
 	public void editAlbum(String newAlbumName, String oldAlbumName) {
-		
+
 		albumController.editAlbum(newAlbumName, oldAlbumName, user);
 	}
+
 	public void logout() {
-		
+
 		userController.logout(user);
 	}
-	
+
 	public boolean deleteAlbum(String albumName) {
-		
+
 		return albumController.deleteAlbum(albumName, user);
 	}
+
 	public Collection<IAlbum> listAlbums() {
 		Collection<IAlbum> albums = albumController.listAlbums(user);
 
 		return albums;
 	}
-	
-	public SortedSet<IPhoto> getPhotosByTag(List<String> tagNames, List<String> tagValues) {
-		
-		return photoController.getPhotosByTag(tagNames, tagValues, user);
-	}
-	
-	public SortedSet<IPhoto> getPhotosByDate(Calendar start, Calendar end) {
-		
-		return photoController.getPhotosByDate(start, end, user);
-	}
-	public boolean saveUser(){
+
+	public boolean saveUser() {
 		return userController.writeUser(user);
+	}
+
+	public IAlbum getAlbum() {
+		return album;
+	}
+
+	public void setAlbum(IAlbum album) {
+		this.album = album;
+		
+		if(album != null) {
+			this.photos = new ArrayList<IPhoto>(album.getPhotos());
+		} else {
+			this.photos = null;
+		}
+	}
+
+	public List<IPhoto> getPhotos() {
+		return photos;
+	}
+
+	public void setPhotos(Collection<IPhoto> photos) {
+		this.photos = new ArrayList<IPhoto>(photos);
+	}
+
+	public void setUser(IUser user) {
+		this.user = user;
 	}
 
 }
